eb73cd5719a64692f4670a7d9c03a675a6552cef
Tim Ekl <lithium3141@gmail.com>
2013-07-09 07:43:35 -0700

    Initial Commit

---
+++ TECollectionViewDemo/TEAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TEAppDelegate.h
+//  TECollectionViewDemo
+//
+//  Created by Tim Ekl on 7/9/13.
+//  Copyright (c) 2013 Tim Ekl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TEAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TECollectionViewDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TECollectionViewDemo
+//
+//  Created by Tim Ekl on 7/9/13.
+//  Copyright (c) 2013 Tim Ekl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TEAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TEAppDelegate class]));
+    }
+}
