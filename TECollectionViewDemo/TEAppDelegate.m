b18042788032713c5c54440d931d4a617aa3bbf0
Tim Ekl <lithium3141@gmail.com>
2013-07-09 08:40:42 -0700

    Stub out framework for showing collection views
    Signed-off-by: Tim Ekl <lithium3141@gmail.com>

--- TECollectionViewDemo/TEAppDelegate.m
+++ TECollectionViewDemo/TEAppDelegate.m
@@ -8,42 +8,92 @@
 
 #import "TEAppDelegate.h"
 
+#import "TEColorList.h"
+#import "UIColor+TEExtensions.h"
+
+#define DEBUG_COLOR_MULTIPLIER 30
+
+@interface TEAppDelegate ()
+
+@property (nonatomic, strong) UITabBarController *tabBarController;
+
+@end
+
+@interface TECollectionViewInformation : NSObject
+
+@property (nonatomic, assign) Class controllerClass;
+@property (nonatomic, assign) Class layoutClass;
+@property (nonatomic, copy) NSString *title;
+
++ (instancetype)informationWithController:(Class)controllerClass layout:(Class)layoutClass title:(NSString *)title;
+- (UINavigationController *)wrappedController;
+
+@end
+
 @implementation TEAppDelegate
+
+- (NSArray *)controllerInfo;
+{
+    return @[  ];
+}
+
+- (void)addRandomColor;
+{
+    for (NSUInteger i = 0; i < DEBUG_COLOR_MULTIPLIER; i++) {
+        NSIndexPath *newIndexPath = [NSIndexPath indexPathForItem:[[[TEColorList sharedList] allColors] count] inSection:0];
+        [[TEColorList sharedList] addColor:[UIColor randomOpaqueColor]];
+        
+        UINavigationController *navController = (UINavigationController *)[self.tabBarController selectedViewController];
+        UICollectionViewController *collectionViewController = [navController childViewControllers][0];
+        
+        [[collectionViewController collectionView] insertItemsAtIndexPaths:@[ newIndexPath ]];
+    }
+}
+
+#pragma mark - UIApplicationDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+    NSMutableArray *controllers = [NSMutableArray arrayWithCapacity:[[self controllerInfo] count]];
+    [[self controllerInfo] enumerateObjectsUsingBlock:^(id obj, NSUInteger idx, BOOL *stop) {
+        NSParameterAssert([obj isKindOfClass:[TECollectionViewInformation class]]);
+        [controllers addObject:[(TECollectionViewInformation *)obj wrappedController]];
+    }];
+    
+    self.tabBarController = [[UITabBarController alloc] init];
+    [self.tabBarController setViewControllers:controllers animated:NO];
+    
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
-    // Override point for customization after application launch.
     self.window.backgroundColor = [UIColor whiteColor];
+    self.window.rootViewController = self.tabBarController;
     [self.window makeKeyAndVisible];
+    
     return YES;
 }
 
-- (void)applicationWillResignActive:(UIApplication *)application
+@end
+
+@implementation TECollectionViewInformation
+
++ (instancetype)informationWithController:(Class)controllerClass layout:(Class)layoutClass title:(NSString *)title;
 {
-    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-    // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+    TECollectionViewInformation *result = [[self alloc] init];
+    result.controllerClass = controllerClass;
+    result.layoutClass = layoutClass;
+    result.title = title;
+    return result;
 }
 
-- (void)applicationDidEnterBackground:(UIApplication *)application
+- (UINavigationController *)wrappedController;
 {
-    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
-    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-}
-
-- (void)applicationWillEnterForeground:(UIApplication *)application
-{
-    // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
-}
-
-- (void)applicationDidBecomeActive:(UIApplication *)application
-{
-    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-}
-
-- (void)applicationWillTerminate:(UIApplication *)application
-{
-    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    UICollectionViewController *collectionViewController = [[self.controllerClass alloc] initWithCollectionViewLayout:[[self.layoutClass alloc] init]];
+    collectionViewController.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemAdd
+                                                                                                               target:(TEAppDelegate *)[[UIApplication sharedApplication] delegate]
+                                                                                                               action:@selector(addRandomColor)];
+    collectionViewController.title = self.title;
+    
+    UINavigationController *navController = [[UINavigationController alloc] initWithRootViewController:collectionViewController];
+    return navController;
 }
 
 @end
